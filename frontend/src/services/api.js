4541cecd62e3b8803c4799bc073b97b8ac937855
Vignesh-089 <solaivignesh60@gmail.com>
2026-03-10 22:00:36 +0530

    frontend deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api", // change if needed
+// });
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change if needed
+  baseURL: "https://cms-mliy.onrender.com/api", // change if needed
 });
 
 // 🔐 Attach token automatically
